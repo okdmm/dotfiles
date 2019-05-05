16b7c1d7cf40b4bf620daa021cb1307f44312598
okada <kirinnremonn1122@gmail.com>
2019-05-05 22:54:10 +0900

    add initias setting of emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,30 @@
+;;; init.el
+
+(package-initialize)
+
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("melpa" . "http://melpa.org/packages/")
+        ("org" . "http://orgmode.org/elpa/")))
+
+(unless package-archive-contents (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(use-package init-loader
+             :ensure t
+             :config
+             (init-loader-load "~/.emacs.d/inits"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (init-loader use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ .emacs.d/inits/05keybinding.el
@@ -0,0 +1,8 @@
+; key bind
+
+;; C-h
+(global-set-key "\C-h" 'delete-backward-char)
+;; help
+(global-set-key "\C-c\C-h" 'help-command)
+;; cemment out
+(global-set-key "\C-c;" 'comment-dwim)
