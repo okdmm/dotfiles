0bc1450e81fcab3fa86e80e4570382c7db8de115
okada <kirinnremonn1122@gmail.com>
2019-05-06 21:20:12 +0900

    jj to esc

--- .emacs.d/inits/30evil.el
+++ .emacs.d/inits/30evil.el
@@ -2,24 +2,40 @@
              :ensure t
              :defer .1
              :init
-             (setq evil-want-integration nil) ;; required by evil-collection
              (setq evil-search-module 'evil-search)
              (setq evil-ex-complete-emacs-commands nil)
              (setq evil-vsplit-window-right t) ;; like vim's 'splitright'
              (setq evil-split-window-below t) ;; like vim's 'splitbelow'
              (setq evil-shift-round nil)
              (setq evil-want-C-u-scroll t)
+             (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
+             (setq evil-want-keybinding nil)
              :config
              (evil-mode)
+             (define-key evil-insert-state-map "jj" `evil-normal-state)
              )
 
-(use-package evil-matchit
-  ;;press "%" to jump between tag pair
-  :config
-  (global-evil-matchit-mode 1))
+(use-package evil-collection
+             :after evil
+             :ensure t
+             :config
+             (evil-collection-init))
 
-  (use-package evil-collection
-    :after evil
-    :ensure t
-    :config
-    (evil-collection-init))
+(use-package evil-matchit
+             ;;press "%" to jump between tag pair
+             :ensure t
+             :config
+             (global-evil-matchit-mode 1))
+
+(use-package evil-surround
+             :ensure t
+             :commands
+             (evil-surround-edit
+               evil-Surround-edit
+               evil-surround-region
+               evil-Surround-region)
+             :init
+             (evil-define-key 'operator global-map "s" 'evil-surround-edit)
+             (evil-define-key 'operator global-map "S" 'evil-Surround-edit)
+             (evil-define-key 'visual global-map "S" 'evil-surround-region)
+             (evil-define-key 'visual global-map "gS" 'evil-Surround-region))
